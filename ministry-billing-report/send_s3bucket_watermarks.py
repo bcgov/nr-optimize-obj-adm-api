0d42de08fde0366406482cd3c8604460d9f7c285
Peter Platten <peter.platten@gov.bc.ca>
2021-08-10 16:56:01 -0700

    file name changes, added draft of python db query

--- ministry-billing-report/send_s3bucket_watermarks.py
+++ ministry-billing-report/send_s3bucket_watermarks.py
@@ -150,6 +150,35 @@ def main():
         buckets_dict = get_buckets(constants.OBJSTOR_MGMT_NAMESPACE, client)
         buckets_dict = buckets_dict
     send_csv(constants.DEBUG_EMAIL)
+    """
+    conn = None
+    try:
+        # Open a connection
+        conn = psycopg2.connect(
+            host="postgresql",
+            database="metabase",
+            user="metabaseps",
+            password=""
+        )
+        # create a cursor
+            cur = conn.cursor()
+
+        # execute a statement
+            print('PostgreSQL database version:')
+            cur.execute('SELECT version()')
+    # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+    # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+        """
 
 
 if __name__ == "__main__":
