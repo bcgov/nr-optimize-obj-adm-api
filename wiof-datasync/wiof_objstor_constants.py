c9e065f034e4142b8c1ffb034986a73aae493f4c
Joseph <58308962+jmontebe@users.noreply.github.com>
2021-08-26 11:59:40 -0700

    Update wiof_objstor_constants.py

--- wiof-datasync/wiof_objstor_constants.py
+++ wiof-datasync/wiof_objstor_constants.py
@@ -1,12 +1,4 @@
 import os
-import dotenv
-
-envPath = os.path.join(os.path.dirname(__file__), "..", ".env")
-if os.path.exists(envPath):
-    print("loading dot env...")
-    dotenv.load_dotenv()
-else:
-    print(".env file not found")
 
 OBJSTOR_ACCESS_KEY = os.environ["OBJSTOR_ACCESS_KEY"]
 OBJSTOR_SECRET_KEY = os.environ["OBJSTOR_SECRET_KEY"]
