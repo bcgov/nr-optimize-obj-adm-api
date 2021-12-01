c7a03b2ceaee986866810d040873bd43efb292a3
jmontebe <joseph.montebello@gov.bc.ca>
2021-12-01 08:57:44 -0800

    Update to remove debugging messages

--- wiof_datasync/wiof_objstor_datasync.py
+++ wiof_datasync/wiof_objstor_datasync.py
@@ -67,7 +67,7 @@ def main(argv):
     )
     for bucket_file in bucket_files:
         file_name = bucket_file.object_name
-        print(f"file_name: {file_name}")
+        # print(f"file_name: {file_name}")
         if bucket_file.last_modified is None:
             print(f"file {bucket_file} missing last_modified")
             continue
@@ -136,7 +136,7 @@ def main(argv):
         use_url_encoding_type=False,
     )
     for bucket_file in bucket_files:
-        print(f"file_name: {bucket_file}")
+        # print(f"file_name: {bucket_file}")
         if bucket_file.last_modified is None:
             print(f"file {bucket_file} missing last_modified")
             continue
