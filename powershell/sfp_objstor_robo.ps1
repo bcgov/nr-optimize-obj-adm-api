f7ff7316e1d79a39150e3ea2a2e77f4430a1f24c
Heather Hay <Heather.Hay@gov.bc.ca>
2024-01-31 14:03:29 -0800

    beginnings of a script

--- powershell/sfp_objstor_robo.ps1
+++ powershell/sfp_objstor_robo.ps1
@@ -1,9 +1,12 @@
 # -------------------------------------------------------------------------------
 # Name:        sfp_objstor_robo.ps1
-# Purpose:     the purpose of the script is to create a folder hierachy in GeoDrive that matches the client's existing SFP structure, THEN apply ACLs, THEN copy the files into the folders.
+# Purpose:     the purpose of the script is to create a folder hierachy in GeoDrive 
+#              that matches the client's existing SFP structure, THEN apply ACLs, THEN copy the files into the folders.
 #              1.) For each folder:
-#		        a.) If destination is a root folder, apply folder ACLs to the directory where the ACLs were inherited on the origin and where inheritance IS ContainerInherit.
-#               b.)If destination is a root folder, apply folder ACLs to the directory where the ACLs were inherited on the origin and where inheritance IS NOT ContainerInherit.
+#		        a.) If destination is a root folder, apply folder ACLs to the directory 
+#                   where the ACLs were inherited on the origin and where inheritance IS ContainerInherit.
+#               b.) If destination is a root folder, apply folder ACLs to the directory 
+#                   where the ACLs were inherited on the origin and where inheritance IS NOT ContainerInherit.
 #               c.) Apply non-inherited folder ACLs where inheritance IS  ContainerInherit
 #               d.) Apply non-inherited folder ACLs where inheritance IS NOT ContainerInherit
 #              2.) For each file:
@@ -23,3 +26,8 @@
 # usage: powershell ./sfp_objstor_robo.ps1
 # example: powershell ./sfp_objstor_robo.ps1
 # -------------------------------------------------------------------------------
+
+# copy folder-only hierarchy from source to destination
+$source = "C:\Users\HHAY\OneDrive - Government of BC\Optimization" # change these later to be input prompts
+$dest = "C:\TEMP"
+Copy-Item $source $dest -Filter {PSIsContainer} -Recurse -Force
